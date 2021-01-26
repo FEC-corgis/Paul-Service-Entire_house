d4a924c349ab3eed169ae95824705df2d522a3ae
Paul Nguyen <paulngu1120@gmail.com>
2021-01-26 21:25:42 +0000

    progress on testing for the night

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -26,7 +26,7 @@ describe('Get Endpoints', ()=> {
     expect(res.body[0].Rules_id).toBe(randomIndex)
     expect(res.body[0].Property_details_id).toBe(randomIndex)
     done()
-  })
+  });
 
   it('should have additional rules information be empty and optional', async done =>{
     const res = await request(app)
@@ -34,7 +34,7 @@ describe('Get Endpoints', ()=> {
     expect(res.status).toBe(200)
     expect(res.body[0].additional_rules).toBeNull()
     done()
-  })
+  });
 
   it('should contain properties of sanitation when hitting end point of sanitation', async done => {
     const res = await request(app)
