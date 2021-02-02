8722235c337eb2d764ec41546035ea43c5c7eb16
Paul Nguyen <paulngu1120@gmail.com>
2021-02-02 21:44:50 +0000

    Create endpoints for Jenny and Slava

--- server/index.js
+++ server/index.js
@@ -13,10 +13,30 @@ app.use(bp.json());
 app.get('/propertyDetails/', async(req, res)=>{
   let val = Object.values(req.query);
   if (val.length > 0) {
-    const [results, metadata]= await sequelize.query(`SELECT * FROM Property_details d, Rules r, Sanitations s WHERE d.id=${val[0]} && r.id=${val[0]} && s.id=${val[0]}`);
+    const [results, metadata] = await sequelize.query(`SELECT * FROM Property_details d, Rules r, Sanitations s WHERE d.id=${val[0]} && r.id=${val[0]} && s.id=${val[0]}`);
+    res.status(200).send(results);
+  }else{
+    const [allResults, meta] = await sequelize.query(`SELECT * FROM Property_details`);
+    res.status(200).send(allResults);
+  }
+});
+
+//For Jenny room type and building
+app.get('/propertyType/', async(req, res)=>{
+  let val = Object.values(req.query);
+  if(val.length>0){
+    const [results, metadata] = await sequelize.query(`SELECT property_type, property_narrow_type FROM Property_details WHERE id=${val[0]}`);
     res.status(200).send(results);
   }
 });
 
+//For Slava guest capacity
+app.get('/propertyType/', async(req, res)=>{
+  let val = Object.values(req.query);
+  if(val.length>0){
+    const [results, metadata] = await sequelize.query(`SELECT guest_capacity FROM Property_details WHERE id=${val[0]}`);
+    res.status(200).send(results);
+  }
+});
 app.listen(port, ()=>console.log('Server is listening on port ' + port)); 
 module.exports = app // for testing purposes
