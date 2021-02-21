ead627c55ab8c162755a5ae4c4520021d988a100
Paul Van <paulyv8n@gmail.com>
2021-02-21 14:54:39 -0800

    added sleeping arrangements and amenities hard code to service

--- client/components/hosted.jsx
+++ client/components/hosted.jsx
@@ -40,9 +40,59 @@ const HostedBy = (props) => {
        </div>
        <div className="sleepingContainer">
           <h1 className="titleForService">Sleeping arrangements</h1>
+          <div className="sleepingArrangements">
+            <img className="hardcodeIMG" src="https://entirehouse-img.s3-us-west-1.amazonaws.com/beds.svg"></img>
+            <h4>Bedroom 1</h4>
+            <p>1 queen bed</p>
+          </div>
        </div>
        <div className="Amenities">
-          <h1 className="titleForService">Amenities</h1>
+         <h1 className="titleForService">Amenities</h1>
+           <div className="amenContainer">
+             <div className="amen">
+               <div>TV</div>
+               <img className="hardcodeIMG" src="https://entirehouse-img.s3-us-west-1.amazonaws.com/tv.svg"></img>
+             </div>
+             <div className="amen">
+               <div>WiFi</div>
+               <img className="hardcodeIMG" src="https://entirehouse-img.s3-us-west-1.amazonaws.com/wifi.svg"></img>
+               </div>
+             <div className="amen">
+               <div>Kitchen</div>
+               <img className="hardcodeIMG"  src="https://entirehouse-img.s3-us-west-1.amazonaws.com/kitchen.svg"></img>
+               </div>
+             <div className="amen">
+               <div>Free parking on premises</div>
+               <img className="hardcodeIMG"  src="https://entirehouse-img.s3-us-west-1.amazonaws.com/parking.svg"></img>
+             </div>
+             <div className="amen">
+               <div>Hot Tub</div>
+               <img className="hardcodeIMG"  src="https://entirehouse-img.s3-us-west-1.amazonaws.com/hottub.svg"></img>
+               </div>
+             <div className="amen">
+               <div>Central Heating</div>
+               <img className="hardcodeIMG" src="https://entirehouse-img.s3-us-west-1.amazonaws.com/heating.svg"></img>
+             </div>
+             <div className="amen">
+               <div>Smoke Alarm</div>
+               <img className="hardcodeIMG" src="https://entirehouse-img.s3-us-west-1.amazonaws.com/smokealarm.svg"></img>
+               </div>
+             <div className="amen">
+               <div>Carbon monoxide alarm</div>
+               <img className="hardcodeIMG" src="https://entirehouse-img.s3-us-west-1.amazonaws.com/carbonalarm.svg"></img>
+             </div>
+             <div className="amen">
+               <div>First aid kit</div>
+               <img className="hardcodeIMG" src="https://entirehouse-img.s3-us-west-1.amazonaws.com/firstaid.svg"></img>
+               </div>
+             <div className="amen">
+               <div>Fire extinguisher</div>
+               <img className="hardcodeIMG" src="https://entirehouse-img.s3-us-west-1.amazonaws.com/fireextingusiher.svg"></img>
+             </div>
+             <div className="showAllAmenContainer">
+               <a href="">Show all 29 amenities</a>
+             </div>
+         </div>
        </div>
       </div>
       ) : entireHost = null;
